83bbc6db4e2962d5b8100de6ce7cad763ec647e1
Matt Jallen <matt.l.jallen@gmail.com>
2016-10-20 17:40:18 -0500

    Camera function works. Uses the devices camera application to take pictures stores them to the devices storage

--- app/src/main/java/edu/umn/trashmapper/MainActivity.java
+++ app/src/main/java/edu/umn/trashmapper/MainActivity.java
@@ -1,7 +1,23 @@
 package edu.umn.trashmapper;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static edu.umn.trashmapper.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +25,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
+
+    /**
+     * This function is used to open the devices camera
+     * it creates a new intent that can be started by a
+     * button press, and allows users to capture images
+     * and then saves them to the devices storage using
+     * createImageFile() and stores them to
+     * Android/data/edu.umn.trashmapper/files/Pictures"
+     */
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Context context = getApplicationContext();
+                CharSequence text = "An Error Occurred";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "edu.umn.trashmapper",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    /**
+     * This function creates a file for the picture
+     * with the time and day in the filename
+     */
+    String mCurrentPhotoPath;
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    //This is the picture that the user takes using the camera.
+    private File photoFile = null;
 }
