dff8f726555ddbb8bb528b7eb5a40148bea15b13
Li Kun <lixx4136@d.umn.edu>
2016-10-30 23:07:09 -0500

    Add the TrashDescription Activity. There should be three checkbox to let users choose the trash type and one EditView to collect the reason why they create the garbage.

--- app/src/main/java/edu/umn/trashmapper/UserInformationActivity.java
+++ app/src/main/java/edu/umn/trashmapper/UserInformationActivity.java
@@ -3,21 +3,20 @@ package edu.umn.trashmapper;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -187,6 +186,9 @@ public class UserInformationActivity extends AppCompatActivity implements Loader
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            ////////////////////////
+            Intent intent = new Intent(this, TrashDescription.class);
+            startActivity(intent);
         }
     }
 
@@ -345,6 +347,11 @@ public class UserInformationActivity extends AppCompatActivity implements Loader
             mAuthTask = null;
             showProgress(false);
         }
+    }
+
+    public void switchToDescription(View a){
+        Intent intent = new Intent(this, TrashDescription.class);
+        startActivity(intent);
     }
 }
 
