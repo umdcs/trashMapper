639f2bf372613c060e8b8154ab45076a6b86e626
Matt Jallen <matt.l.jallen@gmail.com>
2016-11-01 09:59:37 -0500

    Fixed gallery permissions

--- app/src/main/java/edu/umn/trashmapper/TrashDescription.java
+++ app/src/main/java/edu/umn/trashmapper/TrashDescription.java
@@ -9,7 +9,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 import android.Manifest;
 import java.io.File;
 import java.io.IOException;
+import java.security.Permissions;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -52,9 +55,30 @@ public class TrashDescription extends AppCompatActivity {
         galleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openGallery();
+                //Checks for permissions to read from storage
+                //Requests the permission if it isn't granted and then opens the gallery
+                //else opens the gallery
+                if (ActivityCompat.checkSelfPermission(TrashDescription.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(TrashDescription.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE);
+                }
+                else {
+                    openGallery();
+                }
             }
         });
+    }
+
+    //opens the gallery after permissions granted
+    @Override
+    public void onRequestPermissionsResult(int requestCode,@NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: { //Permissions.READ_CONTACTS is an int constant
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery();
+                }
+            }
+        }
     }
 
     /**
@@ -114,10 +138,9 @@ public class TrashDescription extends AppCompatActivity {
      * internal storage
      */
     private void openGallery(){
-        verifyStoragePermissions(this);
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, PICK_IMAGE);
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, PICK_IMAGE);
     }
 
     /**
@@ -151,27 +174,6 @@ public class TrashDescription extends AppCompatActivity {
         }
     }
 
-    /**
-     * Checks if the app has permission to write to device storage
-     *
-     * If the app does not has permission then the user will be prompted to grant permissions
-     * http://stackoverflow.com/questions/23527767/open-failed-eacces-permission-denied
-     * @param activity
-     */
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
-
 
     public void onCheckboxClicked(View a){
 
@@ -185,8 +187,4 @@ public class TrashDescription extends AppCompatActivity {
 
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
 }
