89a854d4714ed57c66b64928590ffa7d4b49eee1
yichenw1 <weixx470@d.umn.edu>
2016-11-22 00:27:52 -0600

    The DisplayActivity directly get the normal trash&user information from the intent from the MapsActivity, and the trashPicture directly from the server (Using another route handler '/seperate') and display both

--- app/src/main/java/edu/umn/trashmapper/DisplayActivity.java
+++ app/src/main/java/edu/umn/trashmapper/DisplayActivity.java
@@ -11,16 +11,19 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import static android.util.Base64.DEFAULT;
 
-public class DisplayActivity extends AppCompatActivity {
+public class DisplayActivity extends AppCompatActivity implements AsyncResponse {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        httpAsyncTask = new HTTPAsyncTask(this);
         Intent intent = getIntent();
         Bundle extra = intent.getExtras();
         //Get the values of the the messages in the intent from the MapsActivity
@@ -28,32 +31,115 @@ public class DisplayActivity extends AppCompatActivity {
         System.out.println("message is " + message);
         String[] split = message.split("%");
         String normalMessage = split[0];
-        String pictureString = split[1];
+        //String pictureString = split[1];
+        String indexString = split[1];
+        index = Integer.parseInt(indexString);
+       /* if(inter == null)
+        {
+            Log.d("Inter", "inter is null");
+        }
+        else{
+            Log.d("Inter", "inter is not null");
+        }
+        try {
+            obj = inter.getJSONObject(index);
+            pictureString = obj.getString("picture");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } *//*catch (NullPointerException e)
+        {
+            Log.d("NULL", "NULL POINTER EXCEPTION");
+        }*/
+
+        /*if(pictureString == null)
+        {
+            Log.d("pictureString", pictureString);
+        }
+        else{
+            Log.d("pictureString","NOTNULL");
+        }*/
+        // Log.d("DEBUG", sjason.getString("longitude"));
+
+        restGETPhoto();
         //String picture = extra.getString("TRASH_PIC_STRING");
 
+        TextView textView = new TextView(this);
+        textView.setTextSize(15);
+        textView.setText(normalMessage);
+
+       // image = new ImageView(this);
+
+        Log.d("Before", "Haha");
         /*try {
             createFile(pictureString);
         } catch (JSONException e) {
             e.printStackTrace();
         }*/
 
-        TextView textView = new TextView(this);
-        textView.setTextSize(15);
-        textView.setText(normalMessage);
-
+        Log.d("After", "HaHahaha");
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display);
         layout.addView(textView);
+        //layout.addView(image);
+
+        Log.d("Final", "Xixixi");
     }
 
-    /*private void createFile(String encrypted) throws JSONException
+    private void createFile(String encrypted) throws JSONException
     {
         if(encrypted != null)
         {
-            Log.d("Debug", "String is " + encrypted);
+            System.out.println("TEST IS " + test);
+            Log.d("InCreate", "String is " + encrypted);
             byte[] decoded = Base64.decode(encrypted, DEFAULT);
             Bitmap pic = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
-            ImageView image = (ImageView) findViewById(R.id.trash_display);
+            ImageView image = (ImageView) findViewById(R.id.trash);
             image.setImageBitmap(pic);
+            //image.setImageResource(R.drawable.carrot_48);
         }
-    }*/
+        else{
+            Log.d("NULL","String is null");
+        }
+    }
+
+    public void restGETPhoto()
+    {
+        httpAsyncTask.execute("http://131.212.212.94:4321/seperate", "GET");
+        //httpAsyncTask.cancel(true);
+        // new HTTPAsyncTask().execute("http://10.0.2.2:4321/userData/userData", "GET");
+        // new HTTPAsyncTask().execute("https://lempo.d.umn.edu:8193/userData", "GET");
+    }
+
+    @Override
+    public void processFinish(String result) {
+        try
+        {
+            JSONObject bjason = new JSONObject(result);
+            inter = bjason.getJSONArray("items");
+            obj = inter.getJSONObject(index);
+            // Log.d("DEBUG", sjason.getString("longitude"));
+            pictureString = obj.getString("picture");
+            Log.d("asdasdasdasdasDISP", pictureString);
+
+            try {
+                createFile(pictureString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        httpAsyncTask.cancel(true);
+    }
+
+    String pictureString;
+    HTTPAsyncTask httpAsyncTask;
+    JSONObject obj;
+    JSONArray inter;
+    int index;
+    ImageView image;
+    String test;
 }
