26d4f50a95b488e8f0997be265dee9a9d7ad57d3
yichenw1 <weixx470@d.umn.edu>
2016-12-06 00:27:18 -0600

    Change the address to lempo

--- app/src/main/java/edu/umn/trashmapper/HTTPAsyncTask.java
+++ app/src/main/java/edu/umn/trashmapper/HTTPAsyncTask.java
@@ -97,6 +97,7 @@ class HTTPAsyncTask extends AsyncTask<String, Integer, String>
     }
 
     public AsyncResponse delegate;
-    public String address = "http://131.212.220.81:4321";
+    public String address = "\n" +
+            "https://lempo.d.umn.edu:8193";
 
 }
